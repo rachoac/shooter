a0836bf0f5d91447a2c8cbb393b27dea2776cb3e
aron <aron@olioapps.com>
2019-05-25 15:44:58 -0700

    adding nucleus of client2

---
+++ client2/src/sketch.ts
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight)
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background(100);
+    ellipse(10, 10, 10, 10)
+}
